5dc497436f562ac128465a335925c892d757b0d3
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2021-11-06 03:27:37 +0800

    list to array

---
+++ src/main/java/com/example/kata/IntArrayToList.java
@@ -0,0 +1,25 @@
+package com.example.kata;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * author: rt
+ * Copyright (c) 2021 by rt
+ * All rights reserved.
+ */
+
+public class IntArrayToList {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 5};
+        System.out.println(Arrays.toString(array)); //[1, 2, 3, 5]
+
+
+        List<Integer> list = Arrays.stream(array)
+                .boxed()
+                .collect(Collectors.toList());
+
+        System.out.println(list); //[1, 2, 3, 5]
+    }
+}
