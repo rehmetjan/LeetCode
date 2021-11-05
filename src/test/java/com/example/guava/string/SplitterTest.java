10d540835166133db4e2609c8bf6a89401529d80
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2021-11-06 03:03:34 +0800

    guava library

---
+++ src/test/java/com/example/guava/string/SplitterTest.java
@@ -0,0 +1,26 @@
+package com.example.guava.string;
+
+import com.google.common.base.Splitter;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * author: rt
+ * Copyright (c) 2021 by rt
+ * All rights reserved.
+ */
+public class SplitterTest {
+
+    @Test
+    public void testSplitter() {
+        List<String> stringList = Splitter.on(".").splitToList("net.selte.base");
+        for (String s : stringList) {
+            System.out.println(s);
+        }
+
+        assertEquals("net", stringList.get(0));
+    }
+}
