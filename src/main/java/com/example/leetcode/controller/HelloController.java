2a8d201a4002afe64d6e43f193608f71ad4730bb
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2021-11-06 04:40:16 +0800

    thymeleaf added, FindSmallestInt ans some katas

---
+++ src/main/java/com/example/leetcode/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.leetcode.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * author: rt
+ * Copyright (c) 2021 by rt
+ * All rights reserved.
+ */
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
