4725b09f3e9ae117c4c8e950de376d4ccc7ca59b
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2021-11-07 22:12:15 +0800

    Move zeroes

---
+++ src/test/java/com/example/coding/problems/leetcode/MoveZeroesTest.java
@@ -0,0 +1,30 @@
+package com.example.coding.problems.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * author: rt
+ * Copyright (c) 2021 by rt
+ * All rights reserved.
+ */
+class MoveZeroesTest {
+
+    @Test
+    public void test1() {
+        MoveZeroes moveZeroes = new MoveZeroes();
+        int[] nums = new int[]{0, 2, 0, 4, 1};
+        moveZeroes.moveZeroes(nums);
+        Assertions.assertArrayEquals(new int[]{2, 4, 1, 0, 0}, nums);
+    }
+
+    @Test
+    public void test2() {
+        MoveZeroes moveZeroes = new MoveZeroes();
+        int[] nums = new int[]{0, 1, 0, 3, 12};
+        moveZeroes.moveZeroes(nums);
+        Assertions.assertArrayEquals(new int[]{1, 3, 12, 0, 0}, nums);
+    }
+
+
+}
