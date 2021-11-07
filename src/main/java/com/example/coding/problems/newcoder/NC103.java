b9208bf0a4f8f25ca9ce1ded3b5f0e837855cec2
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2021-11-07 20:52:11 +0800

    new coder reverse text

---
+++ src/main/java/com/example/coding/problems/newcoder/NC103.java
@@ -0,0 +1,22 @@
+package com.example.coding.problems.newcoder;
+
+/**
+ * author: rt
+ * Copyright (c) 2021 by rt
+ * All rights reserved.
+ */
+public class NC103 {
+    public static String solve(String str) {
+        char[] ans = str.toCharArray();
+        int length = str.length();
+        for (int i = 0; i < length; i++) {
+            ans[i] = str.charAt(length - 1 - i);
+        }
+        return new String(ans);
+    }
+
+    public static String solve2(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        return sb.reverse().toString();
+    }
+}
