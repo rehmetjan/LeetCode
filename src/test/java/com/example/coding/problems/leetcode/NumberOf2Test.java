316c52bc1b250c7e1bf36f5775965be566d560f1
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2021-11-08 18:35:21 +0800

    SearchInsert and NumberOf2

---
+++ src/test/java/com/example/coding/problems/leetcode/NumberOf2Test.java
@@ -0,0 +1,19 @@
+package com.example.coding.problems.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * author: rt
+ * Copyright (c) 2021 by rt
+ * All rights reserved.
+ */
+class NumberOf2Test {
+
+    @Test
+    public void test1() {
+        int result = NumberOf2.numberOf2sInRange(25);
+        assertEquals(9, result);
+    }
+}
