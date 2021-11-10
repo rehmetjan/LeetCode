e445276a1e4d3c3cda41caf5d80e5e7dd9099f79
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2021-11-11 01:48:03 +0800

    LeetCodeApplication

--- src/main/java/com/example/coding/LeetCodeApplication.java
+++ src/main/java/com/example/coding/LeetCodeApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LeetcodeApplication {
-
+public class LeetCodeApplication {
     public static void main(String[] args) {
-        SpringApplication.run(LeetcodeApplication.class, args);
+        SpringApplication.run(LeetCodeApplication.class, args);
     }
-
 }
