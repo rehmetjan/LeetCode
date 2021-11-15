76417e0eac2a9ce3a27a0960120a1e0a286097c6
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2021-11-15 19:16:45 +0800

    ClimbingStairs

---
+++ src/main/java/com/example/coding/problems/leetcode/ClimbingStairs.java
@@ -0,0 +1,52 @@
+package com.example.coding.problems.leetcode;
+
+/**
+ * author: rt
+ * Copyright (c) 2021 by rt
+ * All rights reserved.
+ * <p>
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 注意：给定 n 是一个正整数。
+ * <p>
+ * 示例 1：
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+ * 示例 2：
+ * <p>
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/climbing-stairs
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        int p, q = 0, r = 1;
+        for (int i = 1; i <= n; ++i) {
+            p = q;
+            q = r;
+            r = p + q;
+        }
+        return r;
+    }
+
+    public static void main(String[] args) {
+        ClimbingStairs cs = new ClimbingStairs();
+        System.out.println(cs.climbStairs(10));
+        System.out.println(cs.climbStairs(4));
+        System.out.println(cs.climbStairs(5));
+        System.out.println(cs.climbStairs(6));
+        System.out.println(cs.climbStairs(7));
+    }
+
+}
