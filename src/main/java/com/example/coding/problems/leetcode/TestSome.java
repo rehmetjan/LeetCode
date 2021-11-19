e2ee40c78a3d293de0a096adca1e2310cd14bfcc
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2021-11-19 20:16:28 +0800

    MergeSortedArrays

---
+++ src/main/java/com/example/coding/problems/leetcode/TestSome.java
@@ -0,0 +1,20 @@
+package com.example.coding.problems.leetcode;
+
+/**
+ * author: rt
+ * Copyright (c) 2021 by rt
+ * All rights reserved.
+ */
+
+public class TestSome {
+    public static void main(String[] args) {
+
+        char[] charArray = "Hello World".toCharArray();
+        for (char c : charArray) {
+            for (String s : "Hello World".split("")) {
+                System.out.println(s+s+c);
+            }
+        }
+
+    }
+}
