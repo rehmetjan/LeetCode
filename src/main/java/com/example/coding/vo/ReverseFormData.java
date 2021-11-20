8cab195d8be02661bab39398d7f96ab8cc2da6a4
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2021-11-20 10:24:04 +0800

    ReverseFormData

--- src/main/java/com/example/coding/vo/ReverseFormData.java
+++ src/main/java/com/example/coding/vo/ReverseFormData.java
@@ -1,12 +1,11 @@
 package com.example.coding.vo;
 
-/**
- * author: rt
- * Copyright (c) 2021 by rt
- * All rights reserved.
- */
 public class ReverseFormData {
     private int number;
+
+    public ReverseFormData(int number) {
+        this.number = number;
+    }
 
     public int getNumber() {
         return number;
@@ -14,5 +13,21 @@ public class ReverseFormData {
 
     public void setNumber(int number) {
         this.number = number;
+    }
+
+    public static void main(String[] args) {
+        ReverseFormData formData = new ReverseFormData(3);
+        System.out.println(formData.getNumber());
+        System.out.println(formData.number);
+        formData.setNumber(5);
+        System.out.println(formData.getNumber());
+        System.out.println(formData.number);
+        int i = formData.number;
+        i++;
+        System.out.println(i);
+        ++i;
+        System.out.println(i);
+        ++i;
+        System.out.println(i);
     }
 }
