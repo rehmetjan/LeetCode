5ad85eeace893b1144ad07f2016260499656b1bd
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2021-11-23 18:35:16 +0800

    LongestCommonPrefix

--- src/main/java/com/example/coding/problems/leetcode/LongestCommonPrefix.java
+++ src/main/java/com/example/coding/problems/leetcode/LongestCommonPrefix.java
@@ -31,6 +31,10 @@ package com.example.coding.problems.leetcode;
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/longest-common-prefix
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * <p>
+ * 执行用时：0 ms ,   在所有 Java 提交中击败了 100.00% 的用户
+ * 内存消耗：36.5 MB, 在所有 Java 提交中击败了 51.13% 的用户
+ * 通过测试用例：123 / 123
  */
 public class LongestCommonPrefix {
 
