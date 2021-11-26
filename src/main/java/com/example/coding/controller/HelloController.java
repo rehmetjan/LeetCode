d37db398740cf71685a49bec2ec8478d58f99e73
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2021-11-26 17:29:15 +0800

    reformat java code

--- src/main/java/com/example/coding/controller/HelloController.java
+++ src/main/java/com/example/coding/controller/HelloController.java
@@ -3,7 +3,6 @@ package com.example.coding.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * author: rt
