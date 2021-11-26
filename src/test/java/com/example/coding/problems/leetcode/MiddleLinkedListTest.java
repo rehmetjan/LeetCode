1f111f305571a542f0132025a22b72f752a4d9c0
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2021-11-26 17:29:27 +0800

    reformat java code

--- src/test/java/com/example/coding/problems/leetcode/MiddleLinkedListTest.java
+++ src/test/java/com/example/coding/problems/leetcode/MiddleLinkedListTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * author: rt
  * Copyright (c) 2021 by rt
  * All rights reserved.
- *
+ * <p>
  * 给定一个头结点为 head 的非空单链表，返回链表的中间结点。
  * <p>
  * 如果有两个中间结点，则返回第二个中间结点。
