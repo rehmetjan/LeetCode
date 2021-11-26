46bd847143979e39f9b822d7b2a9c95485e8dac2
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2021-11-26 17:29:45 +0800

    reformat java code

--- src/main/java/com/example/coding/problems/leetcode/RomanToInt.java
+++ src/main/java/com/example/coding/problems/leetcode/RomanToInt.java
@@ -66,6 +66,31 @@ import java.util.Map;
 
 public class RomanToInt {
 
+    static Map<Character, Integer> symbolValues = new HashMap<Character, Integer>() {{
+        put('I', 1);
+        put('V', 5);
+        put('X', 10);
+        put('L', 50);
+        put('C', 100);
+        put('D', 500);
+        put('M', 1000);
+    }};
+
+    public static int romanToInt(String s) {
+        int ans = 0;
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            int value = symbolValues.get(s.charAt(i));
+            if (i < n - 1 && value < symbolValues.get(s.charAt(i + 1))) {
+                ans -= value;
+            } else {
+                ans += value;
+            }
+        }
+
+        return ans;
+    }
+
     public int romanToInteger(String s) {
         int sum = 0;
         int preNum = getValue(s.charAt(0));
@@ -102,31 +127,5 @@ public class RomanToInt {
             default:
                 return 0;
         }
-    }
-
-
-    static Map<Character, Integer> symbolValues = new HashMap<Character, Integer>() {{
-        put('I', 1);
-        put('V', 5);
-        put('X', 10);
-        put('L', 50);
-        put('C', 100);
-        put('D', 500);
-        put('M', 1000);
-    }};
-
-    public static int romanToInt(String s) {
-        int ans = 0;
-        int n = s.length();
-        for (int i = 0; i < n; ++i) {
-            int value = symbolValues.get(s.charAt(i));
-            if (i < n - 1 && value < symbolValues.get(s.charAt(i + 1))) {
-                ans -= value;
-            } else {
-                ans += value;
-            }
-        }
-
-        return ans;
     }
 }
