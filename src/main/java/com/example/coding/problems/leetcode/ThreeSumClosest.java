1017f774462c2f1760fca24d3e1836c4ab39500c
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2021-11-28 23:36:37 +0800

    ThreeSumClosest

---
+++ src/main/java/com/example/coding/problems/leetcode/ThreeSumClosest.java
@@ -0,0 +1,9 @@
+package com.example.coding.problems.leetcode;
+
+/**
+ * author: rt
+ * Copyright (c) 2021 by rt
+ * All rights reserved.
+ */
+public class ThreeSumClosest {
+}
---
+++ src/test/java/com/example/coding/problems/leetcode/ThreeSumClosestTest.java
@@ -0,0 +1,12 @@
+package com.example.coding.problems.leetcode;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * author: rt
+ * Copyright (c) 2021 by rt
+ * All rights reserved.
+ */
+class ThreeSumClosestTest {
+
+}
