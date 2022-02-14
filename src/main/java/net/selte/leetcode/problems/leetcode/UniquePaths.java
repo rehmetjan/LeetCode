651edf3b35726ddbb00d78322c2111a794b7a7ff
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2022-02-14 19:07:52 +0800

    UniquePaths System.out.println(Arrays.deepToString(f)))

--- src/main/java/net/selte/leetcode/problems/leetcode/UniquePaths.java
+++ src/main/java/net/selte/leetcode/problems/leetcode/UniquePaths.java
@@ -1,5 +1,7 @@
 package net.selte.leetcode.problems.leetcode;
 
+import java.util.Arrays;
+
 /**
  * @author rt
  * Copyright (c) 2022 by rt
@@ -8,15 +10,21 @@ package net.selte.leetcode.problems.leetcode;
 public class UniquePaths {
     public int uniquePaths(int m, int n) {
         int[][] f = new int[m][n];
+//        System.out.println("f[i][0]");
         for (int i = 0; i < m; ++i) {
             f[i][0] = 1;
+//            System.out.println(Arrays.deepToString(f));
         }
+//        System.out.println("f[0][j]");
         for (int j = 0; j < n; ++j) {
             f[0][j] = 1;
+//            System.out.println(Arrays.deepToString(f));
         }
+//        System.out.println("f[i][j]");
         for (int i = 1; i < m; ++i) {
             for (int j = 1; j < n; ++j) {
                 f[i][j] = f[i - 1][j] + f[i][j - 1];
+                System.out.println(Arrays.deepToString(f));
             }
         }
         return f[m - 1][n - 1];
