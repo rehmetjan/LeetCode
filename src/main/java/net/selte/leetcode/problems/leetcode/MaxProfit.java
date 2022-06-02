28efb2f5ce2b48a224434a539fa4e99e071f2a3e
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2022-06-02 19:42:48 +0800

    MaxProfit

--- src/main/java/net/selte/leetcode/problems/leetcode/MaxProfit.java
+++ src/main/java/net/selte/leetcode/problems/leetcode/MaxProfit.java
@@ -1,5 +1,8 @@
 package net.selte.leetcode.problems.leetcode;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 /**
  * @author rt
  * Copyright (c) 2022 by rt
@@ -8,13 +11,11 @@ package net.selte.leetcode.problems.leetcode;
 public class MaxProfit {
 
     /**
-     * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+     * 给定一个数组 prices ，它的第i 个元素prices[i] 表示一支给定股票第 i 天的价格。
      *
      * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
      *
      * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
-     *
-     *  
      *
      * 示例 1：
      *
@@ -27,16 +28,14 @@ public class MaxProfit {
      * 输入：prices = [7,6,4,3,1]
      * 输出：0
      * 解释：在这种情况下, 没有交易完成, 所以最大利润为 0。
-     *  
      *
      * 提示：
      *
      * 1 <= prices.length <= 105
      * 0 <= prices[i] <= 104
      *
-     *
      * 来源：力扣（LeetCode）
-     * 链接：<a href="https://leetcode.cn/problems/best-time-to-buy-and-sell-stock">Links</a>
+     * 链接：<a href="https://leetcode.cn/problems/best-time-to-buy-and-sell-stock">https://leetcode.cn/problems/best-time-to-buy-and-sell-stock</a>
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      */
 
@@ -51,5 +50,13 @@ public class MaxProfit {
             }
         }
         return maxprofit;
+    }
+
+    public static void main(String[] args) {
+        Date date = new Date();
+        Timestamp timestamp = new Timestamp(date.getTime());
+        System.out.println(date.toInstant());
+        System.out.println(timestamp);
+        System.out.println(System.currentTimeMillis());
     }
 }
