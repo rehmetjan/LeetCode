3685f467b7c622792a705f9a4f6a73388b4498d8
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2022-07-20 16:28:33 +0800

    Triangle

--- src/main/java/net/selte/leetcode/problems/leetcode/Pyramid.java
+++ src/main/java/net/selte/leetcode/problems/leetcode/Pyramid.java
@@ -1,18 +1,40 @@
 package net.selte.leetcode.problems.leetcode;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author rt
  * Copyright (c) 2022 by rt
  * All rights reserved.
  * I was asked to draw a number triangle using for loop in the interview.
  */
+@Slf4j
 public class Pyramid {
 
     public static void main(String[] args) {
         Pyramid pyramid = new Pyramid();
+
+        log.info("====== 1st method =====");
         pyramid.makePyramid();
+        log.info("====== 2nd method =====");
+        pyramid.make2ndPyramid();
+        log.info("====== third method =====");
+        pyramid.make3rdPyramid();
+
     }
 
+
+    /**
+     * 1
+     * 2 2
+     * 3 3 3
+     * 4 4 4 4
+     * 5 5 5 5 5
+     * 6 6 6 6 6 6
+     * 7 7 7 7 7 7 7
+     * 8 8 8 8 8 8 8 8
+     * 9 9 9 9 9 9 9 9 9
+     */
     public void makePyramid() {
         int i;
         int j;
@@ -24,6 +46,41 @@ public class Pyramid {
                 System.out.print(i);
                 System.out.print(" ");
             }
+            System.out.println();
+        }
+    }
+
+
+    /**
+     * 1
+     * 2 2
+     * 3 3 3
+     * 4 4 4 4
+     */
+    private void make2ndPyramid() {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 4; j > 4 - i; j--) {
+                System.out.print(i);
+                System.out.print(" ");
+            }
+            System.out.println();
+
+        }
+    }
+
+    /**
+     * 1
+     * 1 2
+     * 1 2 3
+     * 1 2 3 4
+     */
+    private void make3rdPyramid() {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(j);
+                System.out.print(" ");
+            }
+            System.out.print(i);
             System.out.println();
         }
     }
