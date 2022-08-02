435342920160ca623555f6b46b35574efa682d66
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2022-08-02 19:52:09 +0800

    implemented the CommandLineRunner

--- src/main/java/net/selte/leetcode/config/StartPreLoading.java
+++ src/main/java/net/selte/leetcode/config/StartPreLoading.java
@@ -2,8 +2,6 @@ package net.selte.leetcode.config;
 
 import lombok.extern.slf4j.Slf4j;
 import net.selte.leetcode.entity.Video;
-import net.selte.leetcode.mapper.VideoMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
