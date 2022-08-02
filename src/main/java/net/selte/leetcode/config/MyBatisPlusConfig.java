56665b820454ef601e22a21e63313b4175451690
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2022-08-02 19:51:56 +0800

    implemented the CommandLineRunner

---
+++ src/main/java/net/selte/leetcode/config/MyBatisPlusConfig.java
@@ -0,0 +1,9 @@
+package net.selte.leetcode.config;
+
+/**
+ * @author rt
+ * Copyright (c) 2022 by rt
+ * All rights reserved.
+ */
+public class MyBatisPlusConfig {
+}
--- src/main/java/net/selte/leetcode/config/StartPreLoading.java
+++ src/main/java/net/selte/leetcode/config/StartPreLoading.java
@@ -1,6 +1,9 @@
 package net.selte.leetcode.config;
 
 import lombok.extern.slf4j.Slf4j;
+import net.selte.leetcode.entity.Video;
+import net.selte.leetcode.mapper.VideoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -14,8 +17,16 @@ import org.springframework.stereotype.Component;
 @Component
 @Order(1)
 public class StartPreLoading implements CommandLineRunner {
+
     @Override
     public void run(String... args) {
         log.info("StartPreLoading ...");
+        for (int i = 0; i < 10; i++) {
+            Video video = new Video();
+            video.setName("Video " + i);
+            video.setSeconds(900L);
+            log.info(video.getName());
+        }
+
     }
 }
