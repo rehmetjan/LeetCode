27ac746a934671241924f600574dc7f9cf8fd01c
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2022-08-17 18:46:49 +0800

    TEST

---
+++ src/main/java/net/selte/leetcode/problems/asm/MyTest.java
@@ -0,0 +1,13 @@
+package net.selte.leetcode.problems.asm;
+
+/**
+ * @author rt
+ * Copyright (c) 2022 by rt
+ * All rights reserved.
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        String a = "Simon";
+        System.out.println(a.length());
+    }
+}
