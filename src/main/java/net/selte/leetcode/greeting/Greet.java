bc046ee86af04ed40503a0ea3f3da5d44d4902ec
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2023-07-17 22:13:34 +0800

    Greet

---
+++ src/main/java/net/selte/leetcode/greeting/Greet.java
@@ -0,0 +1,14 @@
+package net.selte.leetcode.greeting;
+
+import lombok.Data;
+
+/**
+ * @author rt
+ * Copyright (c) 2023 by rt
+ * All rights reserved.
+ */
+@Data
+public class Greet {
+	private String message;
+	private String name;
+}
